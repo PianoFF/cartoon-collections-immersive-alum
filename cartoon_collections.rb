396ee16c13ca34072a76858a5bc7bcab28455922
pianoFF <fangfei1star@gmail.com>
2020-12-14 10:22:18 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(ary)# code an argument here
   # Your code here
+  counter = 1
+  ary.each do |name|
+    puts "#{counter}. #{name}"
+    counter += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(ary)# code an argument here
   # Your code here
+  ary.map{|word|
+    word.capitalize + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(ary)# code an argument here
   # Your code here
+  ary.each{|word|
+    word.length > 4 ? (return true) : next
+  }
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ary)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    ary.include?(cheese) ? (return cheese) : next
+  end
+  return nil 
 end
